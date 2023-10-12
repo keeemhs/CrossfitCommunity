199a1cdbebf00dd23176c0e8dce2a4c778a164aa
keeemhs <alenen0227@naver.com>
2023-10-12 20:54:15 +0900

    error fixed

--- src/pages/HeroWODsDetail.tsx
+++ src/pages/HeroWODsDetail.tsx
@@ -1,18 +1,20 @@
-// HeroWODDetail.js
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { HeroWODsList } from './data/HeroWODsData';
 import dayjs from 'dayjs';
-import './css/HeroWODsDetail.scss'; // 추가
+import './css/HeroWODsDetail.scss';
+import { UserRankList } from './data/UserRankData';
 
 const HeroWODDetail = () => {
     const { id } = useParams();
-
     const heroWod = HeroWODsList.find((board) => board.id === Number(id));
 
     if (!heroWod) {
         return <div>해당 HeroWOD를 찾을 수 없습니다.</div>;
     }
+
+    const userRanks = UserRankList.filter((rank) => rank.activityid === heroWod.id);
+    userRanks.sort((a, b) => a.timecap - b.timecap); // 오름차순 정렬
 
     return (
         <section className="detail">
@@ -31,6 +33,20 @@ const HeroWODDetail = () => {
                 />
                 <p className="hero-wod-info">Time Cap : {heroWod.timeCap}mins</p>
                 <p className="hero-wod-date">Date: {dayjs(heroWod.createdOn).format('YYYY.MM.DD')}</p>
+                <button className="go-back-button" onClick={() => window.history.back()}>
+                    뒤로 가기
+                </button>
+            </div>
+
+            <div className="user-ranks">
+                <h3>User Record</h3>
+                {userRanks.map((userRank, index) => (
+                    <div key={userRank.id} className="user-rank">
+                        <p>
+                            {index + 1}등. {userRank.name} 님의 소요시간: {userRank.timecap}분
+                        </p>
+                    </div>
+                ))}
             </div>
         </section>
     );
---
+++ src/pages/data/UserRankData.tsx
@@ -0,0 +1,45 @@
+export interface UserRank {
+    id: number;
+    activityid: number;
+    name: string;
+    timecap: number;
+}
+
+export const UserRankList: UserRank[] = [
+    {
+        id: 1,
+        activityid: 1,
+        name: 'keeemhs',
+        timecap: 23,
+    },
+    {
+        id: 2,
+        activityid: 3,
+        name: 'keeemhs',
+        timecap: 23,
+    },
+    {
+        id: 3,
+        activityid: 1,
+        name: 'g__gyu_min',
+        timecap: 21,
+    },
+    {
+        id: 4,
+        activityid: 1,
+        name: 'hs_fitter',
+        timecap: 25,
+    },
+    {
+        id: 5,
+        activityid: 2,
+        name: 'g__gyu_min',
+        timecap: 31,
+    },
+    {
+        id: 6,
+        activityid: 2,
+        name: 'hs_fitter',
+        timecap: 25,
+    },
+];
