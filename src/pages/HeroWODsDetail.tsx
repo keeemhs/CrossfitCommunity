9ecb0d4c980fc13bc13ffe13fc46fb3c18f180ca
keeemhs <alenen0227@naver.com>
2023-10-16 15:28:59 +0900

    231016 commit

--- src/pages/HeroWODsDetail.tsx
+++ src/pages/HeroWODsDetail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { HeroWODsList } from './data/HeroWODsData';
 import dayjs from 'dayjs';
@@ -8,13 +8,32 @@ import { UserRankList } from './data/HeroWODsRecord';
 const HeroWODDetail = () => {
     const { id } = useParams();
     const heroWod = HeroWODsList.find((board) => board.id === Number(id));
+    const [name, setName] = useState(''); // 사용자 이름
+    const [time, setTime] = useState(''); // 사용자 기록 시간
+    const [userRankList, setUserRankList] = useState(UserRankList);
 
     if (!heroWod) {
         return <div>해당 HeroWOD를 찾을 수 없습니다.</div>;
     }
 
-    const userRanks = UserRankList.filter((rank) => rank.activityid === heroWod.id);
+    const userRanks = userRankList.filter((rank) => rank.activityid === heroWod.id);
     userRanks.sort((a, b) => a.timecap - b.timecap); // 오름차순 정렬
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const newUserRank = {
+            id: userRankList.length + 1,
+            activityid: heroWod.id,
+            name: name,
+            timecap: parseInt(time),
+        };
+
+        setUserRankList([...userRankList, newUserRank]);
+
+        setName('');
+        setTime('');
+    };
 
     return (
         <section className="detail">
@@ -47,6 +66,12 @@ const HeroWODDetail = () => {
                         </p>
                     </div>
                 ))}
+
+                <form className="record-add" onSubmit={handleSubmit}>
+                    <input className="record-add-input-name" type="text" placeholder="이름" value={name} onChange={(e) => setName(e.target.value)} />
+                    <input className="record-add-input-number" type="number" placeholder="기록 시간(분)" value={time} onChange={(e) => setTime(e.target.value)} />
+                    <button type="submit">기록</button>
+                </form>
             </div>
         </section>
     );
