3d9f07ae25ccbcf33cd08c916655f98f895915bb
keeemhs <alenen0227@naver.com>
2023-10-17 20:30:14 +0900

    231017 commit

--- src/pages/HeroWODsDetail.tsx
+++ src/pages/HeroWODsDetail.tsx
@@ -5,12 +5,22 @@ import dayjs from 'dayjs';
 import './css/HeroWODsDetail.scss';
 import { UserRankList } from './data/HeroWODsRecord';
 
+// UserRank 인터페이스 수정
+export interface UserRank {
+    id: number;
+    activityid: number;
+    name: string;
+    minute?: number;
+    second?: number;
+}
+
 const HeroWODDetail = () => {
     const { id } = useParams<{ id: string }>(); // id 파라미터를 문자열로 타입 지정
     const heroWod = HeroWODsList.find((board) => board.id === Number(id));
     const [name, setName] = useState<string>(''); // 사용자 이름
-    const [time, setTime] = useState<string>(''); // 사용자 기록 시간
-    const [userRankList, setUserRankList] = useState(UserRankList);
+    const [minute, setMinute] = useState<string>(''); // 사용자 기록 시간 (분)
+    const [second, setSecond] = useState<string>(''); // 사용자 기록 시간 (초)
+    const [userRankList, setUserRankList] = useState<UserRank[]>(UserRankList);
 
     // 쿠키에서 username 가져오기
     useEffect(() => {
@@ -35,17 +45,19 @@ const HeroWODDetail = () => {
             return;
         }
 
-        const newUserRank = {
+        const newUserRank: UserRank = {
             id: userRankList.length + 1,
             activityid: heroWod?.id || 0,
             name: name,
-            timecap: parseInt(time),
+            minute: parseInt(minute),
+            second: parseInt(second),
         };
 
         setUserRankList([...userRankList, newUserRank]);
 
         setName('');
-        setTime('');
+        setMinute('');
+        setSecond('');
     };
 
     if (!heroWod) {
@@ -53,15 +65,23 @@ const HeroWODDetail = () => {
     }
 
     const userRanks = userRankList.filter((rank) => rank.activityid === heroWod.id);
-    userRanks.sort((a, b) => a.timecap - b.timecap); // 오름차순 정렬
+    userRanks.sort((a, b) => {
+        const timeA = (a.minute || 0) * 60 + (a.second || 0);
+        const timeB = (b.minute || 0) * 60 + (b.second || 0);
+        return timeA - timeB;
+    });
 
     return (
         <section className="detail">
             <div className="hero-wod-detail">
                 <h2>{heroWod.title}</h2>
-                <p className="hero-wod-info">
-                    {heroWod.round} Rounds {heroWod.type}
-                </p>
+                {heroWod.round && (
+                    <p className="hero-wod-info">
+                        {heroWod.round} Rounds {heroWod.type}
+                    </p>
+                )}
+                {!heroWod.round && heroWod.type && <p className="hero-wod-info">{heroWod.type}</p>}
+
                 <p
                     className="hero-wod-info"
                     dangerouslySetInnerHTML={{
@@ -70,7 +90,7 @@ const HeroWODDetail = () => {
                         }),
                     }}
                 />
-                <p className="hero-wod-info">Weight : {heroWod.weight}</p>
+                {heroWod.weight && <p className="hero-wod-info">Weight : {heroWod.weight}</p>}
                 <p className="hero-wod-info">Time Cap : {heroWod.timeCap}mins</p>
                 <p className="hero-wod-date">Date: {dayjs(heroWod.createdOn).format('YYYY.MM.DD')}</p>
                 <button className="go-back-button" onClick={() => window.history.back()}>
@@ -83,14 +103,28 @@ const HeroWODDetail = () => {
                 {userRanks.map((userRank, index) => (
                     <div key={userRank.id} className="user-rank">
                         <p>
-                            {index + 1}등. {userRank.name} 님의 소요시간: {userRank.timecap}분
+                            Rank{index + 1}. {userRank.name}'s record: {userRank.minute}'{userRank.second}"
                         </p>
                     </div>
                 ))}
 
                 <form className="record-add" onSubmit={handleSubmit}>
                     <input className="record-add-input-name" type="text" placeholder="이름" value={name} onChange={(e) => setName(e.target.value)} readOnly />
-                    <input className="record-add-input-number" type="number" placeholder="기록 시간(분)" value={time} onChange={(e) => setTime(e.target.value)} />
+                    <input className="record-add-input-number" type="number" placeholder="기록(분)" value={minute} onChange={(e) => setMinute(e.target.value)} />
+                    <input
+                        className="record-add-input-number"
+                        type="number"
+                        placeholder="기록(초)"
+                        value={second}
+                        onChange={(e) => {
+                            const value = parseInt(e.target.value, 10); // 입력값을 숫자로 변환
+                            if (!isNaN(value) && value >= 0 && value < 60) {
+                                setSecond(value.toString()); // 숫자로 변환된 값을 문자열로 다시 설정
+                            } else {
+                                setSecond(''); // 유효하지 않은 값이면 빈 문자열로 설정
+                            }
+                        }}
+                    />
                     <button type="submit">기록</button>
                 </form>
             </div>
--- src/pages/data/HeroWODsRecord.tsx
+++ src/pages/data/HeroWODsRecord.tsx
@@ -2,7 +2,8 @@ export interface UserRank {
     id: number;
     activityid: number;
     name: string;
-    timecap: number;
+    minute?: number;
+    second?: number;
 }
 
 export const UserRankList: UserRank[] = [
@@ -10,42 +11,49 @@ export const UserRankList: UserRank[] = [
         id: 1,
         activityid: 2,
         name: 'keeemhs',
-        timecap: 23,
+        minute: 23,
+        second: 4,
     },
     {
         id: 2,
         activityid: 4,
         name: 'keeemhs',
-        timecap: 23,
+        minute: 23,
+        second: 1,
     },
     {
         id: 3,
         activityid: 2,
         name: 'g__gyu_min',
-        timecap: 21,
+        minute: 21,
+        second: 4,
     },
     {
         id: 4,
         activityid: 2,
         name: 'hs_fitter',
-        timecap: 25,
+        minute: 25,
+        second: 4,
     },
     {
         id: 5,
         activityid: 3,
         name: 'g__gyu_min',
-        timecap: 31,
+        minute: 31,
+        second: 4,
     },
     {
         id: 6,
         activityid: 3,
         name: 'hs_fitter',
-        timecap: 25,
+        minute: 25,
+        second: 23,
     },
     {
         id: 7,
         activityid: 7,
         name: 'hs_fitter',
-        timecap: 23,
+        minute: 23,
+        second: 59,
     },
 ];
