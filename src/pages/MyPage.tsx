1ccd5a38597e8bec8a5c9bf61df61c8fd521d9c8
keeemhs <alenen0227@naver.com>
2023-10-17 15:42:55 +0900

    231017 commit

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/MyPage.scss';
 
 const MyPage: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [isValidEmail, setIsValidEmail] = useState(true);
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const inputEmail = e.target.value;
+        setEmail(inputEmail);
+
+        // 정규 표현식을 사용하여 이메일 형식을 확인합니다.
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const isValid = emailRegex.test(inputEmail);
+
+        setIsValidEmail(isValid);
+    };
+
+    const handleLoginClick = () => {
+        if (isValidEmail) {
+            console.log('로그인 버튼이 클릭되었습니다.');
+        } else {
+            alert('올바른 이메일 형식이 아닙니다.');
+        }
+    };
+
     return (
         <div className="wrap">
             <div className="login">
                 <h2>Log-in</h2>
                 <div className="login_id">
                     <h4>E-mail</h4>
-                    <input type="email" name="" id="" placeholder="Email" />
+                    <input type="email" name="" id="" placeholder="Email" value={email} onChange={handleEmailChange} />
+                    {!isValidEmail && (
+                        <p className="error-message" style={{ color: 'red' }}>
+                            올바른 이메일 형식이 아닙니다.
+                        </p>
+                    )}
                 </div>
                 <div className="login_pw">
                     <h4>Password</h4>
@@ -23,7 +50,7 @@ const MyPage: React.FC = () => {
                     </div>
                 </div>
                 <div className="submit">
-                    <button type="submit" value="로그인">
+                    <button type="button" onClick={handleLoginClick}>
                         로그인
                     </button>
                 </div>
